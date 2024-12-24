5eb3373ef43fbf396c81a94e6818ff2038973699
Kaoru <8354804+altoinu@users.noreply.github.com>
2024-12-23 22:08:14 -0500

    Prettier added

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,16 @@
-import globals from "globals";
 import pluginJs from "@eslint/js";
-
+import eslintConfigPrettier from "eslint-config-prettier";
+import globals from "globals";
 
 /** @type {import('eslint').Linter.Config[]} */
-export default [
+const eslintConfig = [
   {
     files: ["**/*.{js,cjs,mjs}"],
     ignores: ["node_modules", "build", "*.css"],
-    languageOptions: { globals: globals.browser }
+    languageOptions: { globals: globals.browser },
   },
   pluginJs.configs.recommended,
+  eslintConfigPrettier,
 ];
+
+export default eslintConfig;
